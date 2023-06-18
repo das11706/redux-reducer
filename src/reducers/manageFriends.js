96ccbbdb049d914cf7ccf504752926357ce298a2
Daniel Suarez <suarez.daniel101@gmail.com>
2023-06-17 22:29:20 -0400

    commit tree e791f4a3005fb3792a815d4b81d020530bd43fbb

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,59 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action){
+  
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return ({
+        ...state,
+         friends: [...state.friends, action.friend]
+        }
+      );
+
+    case "REMOVE_FRIEND":
+      console.log(action.id)
+      const removeIndex = state.friends.findIndex(friend => friend.id === action.id)
+      console.log(removeIndex)
+      return {
+        ...state,
+        friends: [...state.friends.slice(0, removeIndex), ...state.friends.slice(removeIndex + 1)],
+      };
+    default:
+      return state;
+  }
 }
+
+// [2, 7, 9, 40, 100]
+
+// let state = {
+//   friends: []
+// };
+
+// let state = { numberOfPresents: 0 };
+// let action = { type: "INCREASE", };
+
+// export function managePresents(state, action) {
+//   switch (action.type) {
+//     case "INCREASE":
+//       return { numberOfPresents: state.numberOfPresents + 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+
+// let state = {
+//   friend: {
+//     name: "",
+//     hometown: "",
+//     id: "",
+//   }
+// }
+
+// let state = {
+//   friends: [{
+//     friend: {
+//       name: "",
+//       hometown: "",
+//       id: "",
+//     }
+//   }]
+// };
